f68606c787d5f5169e8952c5f8cce2c579475472
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-03-15 13:41:53 +0200

    Change assets

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ burger.addEventListener('click', () => {
 	nav.classList.toggle('nav-open');
 	burger.classList.toggle('toggle');
 });
+
+links.forEach(link => {
+	link.addEventListener('click', () => {
+		nav.classList.toggle('nav-open');
+		burger.classList.toggle('toggle');
+	});
+});
